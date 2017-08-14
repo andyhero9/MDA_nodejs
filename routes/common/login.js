6a1093e9cb52c09f9002e87ebc9d03e8e16dc518
dell-PC\WyethCRM <767046365@qq.com>
2017-08-14 15:25:21 +0800

    添加图片验证码功能

--- routes/common/login.js
+++ routes/common/login.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var captchapng = require('captchapng');
+var sendMail = require('./mail');
 
 /* GET login listing. */
 router.get('/login', function(req, res, next) {
@@ -16,44 +18,51 @@ router.post('/login', function(req, res, next) {
             console.log('getUserbyUsername err:' + err) ;
             return ;
         }
-        if(result[0]) {
-            if (result[0]['password'] == req.body.password) {
-                //cookie&session
-                req.session.sign = true;
-                req.session.uid = result[0]['id'];
-                req.session.username = result[0]['username'];
-                req.session.department = result[0]['department'];
-                req.session.position = result[0]['position'];
-                req.session.email = result[0]['email'];
-                req.session.tel = result[0]['tel'];
-                req.session.type = result[0]['type'];
-                req.session.currentPage = 1;
+        if (req.session.checkcode == req.body.checkcode){
+            if(result[0]) {
+                if (result[0]['password'] == req.body.password) {
+                    //cookie&session
+                    req.session.sign = true;
+                    req.session.uid = result[0]['id'];
+                    req.session.username = result[0]['username'];
+                    req.session.department = result[0]['department'];
+                    req.session.position = result[0]['position'];
+                    req.session.email = result[0]['email'];
+                    req.session.tel = result[0]['tel'];
+                    req.session.type = result[0]['type'];
+                    req.session.currentPage = 1;
                     res.cookie('user', req.body.username, {
-                    maxAge: 10 * 1000,
-                    httpOnly: true
-                });
+                        maxAge: 10 * 1000,
+                        httpOnly: true
+                    });
 
-                if(result[0]['type'] == 'c'){
-                    res.redirect('/list');
-                }
-                if(result[0]['type'] == 'a') {
-                    req.session.states = '全部';
-                    req.session.uPage=1;
-                    res.redirect('/alist');
-                }
+                    if(result[0]['type'] == 'c'){
+                        res.redirect('/list');
+                    }
+                    if(result[0]['type'] == 'a') {
+                        req.session.states = '全部';
+                        req.session.uPage=1;
+                        res.redirect('/alist');
+                    }
 
+                }
+                else
+                {
+                    res.render('login',{
+                        message:"密码错误!"
+                    });
+                }
             }
             else
             {
                 res.render('login',{
-                    message:"密码错误!"
+                    message:"用户不存在!"
                 });
             }
         }
-        else
-        {
+        else{
             res.render('login',{
-                message:"用户不存在!"
+                message:"验证码错误!"
             });
         }
     });
@@ -86,6 +95,19 @@ router.get('/logout', function(req, res, next) {
     }
 })*/
 
+router.get('/checkcode', function(req, res, next) {
+
+    var code = parseInt(Math.random() * 9000 + 1000);
+    req.session.checkcode = code;
+    var p = new captchapng(100, 30, code);
+    p.color(0, 0, 0, 0);
+    p.color(80, 80, 80, 255);
+    var img = p.getBase64();
+    data = {imgbase64:img};
+    res.send(data);
+
+});
+
 router.get('/forget', function(req, res, next) {
     res.render('forgetpass',{
         message:""
@@ -101,6 +123,10 @@ router.post('/forget', function(req, res, next) {
             return ;
         }
         if(result[0]) {
+            var reply = "<h3>会员数据申请系统取回密码</h3>\n" +
+                "<p>邮箱：" + data.email + "</p>\n" +
+                "<p>密码："+ result[0]['password'] +"</p>";
+            sendMail(data.email,'会员数据申请系统取回密码', reply);
             res.render('forgetpass',{
                 message:"密码已发送到邮箱，请查收。"
             });
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,43 @@ var router = express.Router();
 var sendMail = require('./common/mail');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
     //sendMail('767046365@qq.com','这是测试邮件', 'Hi Amor,这是一封测试邮件');
+});*/
+/*var ccap = require('ccap');
+router.get('/', function(req, res, next) {
+    var captcha = ccap({
+        width:190,
+        height:50,
+        offset:30,
+        quality:100,
+        fontsize:40
+/!*        generate:function(){
+            //自定义生成字符串
+            //此方法可不要
+            var str = "qQ";
+            return str;
+        }*!/
+    });
+    var ary = captcha.get();
+    console.log(ary[0]);//字符串
+    console.log(ary[1]);//字符串
+    /!*res.write(ary[1]); //
+    res.end();*!/
+    res.render('index', { title: 'Express',srcads:ary[1] });
+});*/
+
+var captchapng = require('captchapng');
+router.get('/', function(req, res, next) {
+    var code = parseInt(Math.random() * 9000 + 1000);
+    req.session.checkcode = code;
+    var p = new captchapng(100, 30, code);
+    p.color(0, 0, 0, 0);
+    p.color(80, 80, 80, 255);
+    var img = p.getBase64();
+    res.render('index', { title: 'Express',srcads:img });
 });
 
 module.exports = router;
+
